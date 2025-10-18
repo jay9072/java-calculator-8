126cc0b735557b24ccc1538804817a494bbf6cb1
unknown <jay907212@gmail.com>
2025-10-18 19:44:40 +0900

    feat(calculator): return 0 when input is empty

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,4 +1,11 @@
 package calculator;
 
 public class StringCalculator {
+
+    public static int add(String input) {
+        if (input == null || input.trim().isEmpty()) {
+            return 0;
+        }
+    return 0;
+    }
 }
