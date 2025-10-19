96e8dc3c010ee13d9ed4bd23b8234f0e98692a53
unknown <jay907212@gmail.com>
2025-10-20 04:02:11 +0900

    feat(StringCalculator): implement basic custom delimiter parsing (not fully functional)

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -7,9 +7,21 @@ public class StringCalculator {
             return 0;
         }
 
-        String[] numbers = input.split("[,:]");
+        String delimiter = "[,:]";
+        String numbers = input;
+
+        if (input.startsWith("//")) {
+            String[] parts = input.split("\n", 2);
+
+            String customDelimiter = parts[0].substring(2);
+            delimiter = java.util.regex.Pattern.quote(customDelimiter); //문자 그대로 인식하게 하는 안전장치
+            numbers = parts[1];
+        }
+
+        String[] tokens = numbers.split(delimiter);
+
         int sum =0;
-        for (String number : numbers) {
+        for (String number : tokens) {
             sum += Integer.parseInt(number);
         }
 
